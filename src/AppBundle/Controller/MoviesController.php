ba0d53346d00bd27d406518582e82c3a5f0934c1
Maxim Kovalsky <maxim.kovalsky@alfaforex.com>
2018-09-09 21:03:34 +0300

    basic & db: adding new resources with POST method

--- src/AppBundle/Controller/MoviesController.php
+++ src/AppBundle/Controller/MoviesController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Movie;
 use FOS\RestBundle\Controller\ControllerTrait;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
@@ -18,5 +20,19 @@ class MoviesController extends AbstractController
         $movies = $this->getDoctrine()->getRepository('AppBundle:Movie')->findAll();
 
         return $movies;
+    }
+
+    /**
+     * @Rest\View(statusCode=201)
+     * @ParamConverter("movie", converter="fos_rest.request_body")
+     * @Rest\NoRoute()
+     */
+    public function postMoviesAction(Movie $movie)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($movie);
+        $em->flush();
+
+        return $movie;
     }
 }
