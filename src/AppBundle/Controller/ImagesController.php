db468e7b4116a9a56bd0e4be292bb3ee50b6a6de
Maxim Kovalsky <kmakc@yandex.ru>
2018-10-08 15:53:56 +0300

    file: uploading an image (PUT endpoint)

--- src/AppBundle/Controller/ImagesController.php
+++ src/AppBundle/Controller/ImagesController.php
@@ -2,17 +2,51 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Repository\ImageRepository;
+use Symfony\Component\HttpFoundation\File\MimeType\ExtensionGuesser;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\ControllerTrait;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Entity\Image;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
 
 class ImagesController extends AbstractController
 {
 
     use ControllerTrait;
+
+    /**
+     * @var ImageRepository
+     */
+    private $imageRepository;
+
+    /**
+     * @var string
+     */
+    private $imageDirectory;
+
+    /**
+     * @var string
+     */
+    private $imageBaseUrl;
+
+    /**
+     * ImagesController constructor.
+     *
+     * @param ImageRepository $imageRepository
+     * @param string          $imageDirectory
+     * @param string          $imageBaseUrl
+     */
+    public function __construct(ImageRepository $imageRepository, string $imageDirectory, string $imageBaseUrl)
+    {
+        $this->imageRepository = $imageRepository;
+        $this->imageDirectory  = $imageDirectory;
+        $this->imageBaseUrl    = $imageBaseUrl;
+    }
 
     /**
      * @Rest\NoRoute()
@@ -22,9 +56,7 @@ class ImagesController extends AbstractController
      */
     public function postImagesAction(Image $image)
     {
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($image);
-        $em->flush();
+        $this->persistImage($image);
 
         return $this->view($image, Response::HTTP_CREATED)->setHeader(
             'Location',
@@ -33,5 +65,61 @@ class ImagesController extends AbstractController
                 ['image' => $image->getId()]
             )
         );
+    }
+
+    /**
+     * @Rest\NoRoute()
+     */
+    public function putImageUploadAction(?Image $image, Request $request)
+    {
+        if (null === $image) {
+            throw new NotFoundHttpException();
+        }
+
+        // Read the image from request body
+        $content = $request->getContent();
+
+        // Create the temporary file (deleted after request finishes)
+        $tmpfile = tmpfile();
+
+        // Get the temporary file name
+        $tmpFilePath = stream_get_meta_data($tmpfile)['uri'];
+
+        // Write image content to the temporary file
+        file_put_contents($tmpFilePath, $content);
+
+        // Get the file mime-type
+        $finfo    = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($finfo, $tmpFilePath);
+
+        // Check if it's really an image (never trust client set mime-type)
+        if (!in_array($mimeType, ['image/jpeg', 'image/png', 'image/gif'])) {
+            throw new UnsupportedMediaTypeHttpException('File uploaded is not a valid format');
+        }
+
+        // Guess the extension based on mime-type
+        $extensionGuesser = ExtensionGuesser::getInstance();
+
+        // Generate a new random filename
+        $newFileName = md5(uniqid()) . '.' . $extensionGuesser->guess($mimeType);
+
+        // Copy the tmp file to the final uploads directory
+        copy($tmpFilePath, $this->imageDirectory.DIRECTORY_SEPARATOR.$newFileName);
+
+        $image->setUrl($this->imageDirectory . $newFileName);
+
+        $this->persistImage($image);
+
+        return new Response(null, Response::HTTP_OK);
+    }
+
+    /**
+     * @param Image|null $image
+     */
+    protected function persistImage(?Image $image): void
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($image);
+        $em->flush();
     }
 }
